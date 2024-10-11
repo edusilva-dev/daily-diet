b3cf803d72ae2c600d2d9ba247fdbccfdf782b0f
Eduardo Castro <edusilva.dev@gmail.com>
2024-10-11 15:01:25 -0300

    feat: finaliza projeto

--- src/routes/meal/meal.route.ts
+++ src/routes/meal/meal.route.ts
@@ -40,8 +40,28 @@ export async function mealRoutes(app: FastifyInstance) {
     reply.status(200).send({ meals })
   })
 
+  app.get('/meals/:mealId', async (request, reply) => {
+    const { user } = request
+
+    const mealIdParamSchema = z.object({
+      mealId: z.string().uuid(),
+    })
+    const { mealId } = mealIdParamSchema.parse(request.params)
+
+    const meal = await knex('meals')
+      .where({
+        id: mealId,
+        user_id: user!.id,
+      })
+      .first()
+
+    reply.status(200).send({ meal })
+  })
+
   app.put('/meals/:mealId', async (request, reply) => {
-    const deleteMealBodySchema = z.object({
+    const { user } = request
+
+    const mealIdParamSchema = z.object({
       mealId: z.string().uuid(),
     })
 
@@ -52,34 +72,84 @@ export async function mealRoutes(app: FastifyInstance) {
       inDiet: z.boolean().optional(),
     })
 
-    const { mealId } = deleteMealBodySchema.parse(request.params)
+    const { mealId } = mealIdParamSchema.parse(request.params)
 
     const mealToUpdate = updateMealBodySchema.parse(request.body)
 
-    const { user } = request
-
-    await knex('meals')
-      .update(mealToUpdate)
-      .where('id', mealId)
-      .and.where('user_id', user!.id)
+    await knex('meals').update(mealToUpdate).where({
+      id: mealId,
+      user_id: user!.id,
+    })
 
     reply.status(200).send({ message: 'Updated successfully.' })
   })
 
   app.delete('/meals/:mealId', async (request, reply) => {
+    const { user } = request
+
     const deleteMealBodySchema = z.object({
       mealId: z.string().uuid(),
     })
 
     const { mealId } = deleteMealBodySchema.parse(request.params)
 
-    const { user } = request
-
-    await knex('meals')
-      .delete()
-      .where('id', mealId)
-      .and.where('user_id', user!.id)
+    await knex('meals').delete().where({
+      id: mealId,
+      user_id: user!.id,
+    })
 
     reply.status(200).send()
+  })
+
+  app.get('/meals/reports', async (request, reply) => {
+    const { user } = request
+
+    const recordedMeals = await knex('meals')
+      .where({
+        user_id: user!.id,
+      })
+      .orderBy('date', 'desc')
+
+    const mealsInDiet = await knex('meals')
+      .where({
+        user_id: user!.id,
+        in_diet: true,
+      })
+      .count('id', { as: 'mealsInDiet' })
+      .first()
+
+    const mealsOutDiet = await knex('meals')
+      .where({
+        user_id: user!.id,
+        in_diet: false,
+      })
+      .count('id', { as: 'mealsOutDiet' })
+      .first()
+
+    const { bestOnDietSequence } = recordedMeals.reduce(
+      (acc, meal) => {
+        if (meal.in_diet) {
+          acc.currentSequence += 1
+        } else {
+          acc.currentSequence = 0
+        }
+
+        if (acc.currentSequence > acc.bestOnDietSequence) {
+          acc.bestOnDietSequence = acc.currentSequence
+        }
+
+        return acc
+      },
+      { bestOnDietSequence: 0, currentSequence: 0 },
+    )
+
+    reply.status(200).send({
+      reports: {
+        recordedMeals: recordedMeals.length,
+        mealsInDiet: mealsInDiet?.mealsInDiet,
+        mealsOutDiet: mealsOutDiet?.mealsOutDiet,
+        bestOnDietSequence,
+      },
+    })
   })
 }
